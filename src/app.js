5fc502a190c9df5dde75f2c250bfb1e5d515474d
Varad Kamthe <varadk.2704@gmail.com>
2024-08-27 10:12:07 +0530

    basic routing, setup configured

--- src/app.js
+++ src/app.js
@@ -15,6 +15,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js'
 
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// https://localhost:8000/api/v1/users/register
 
 export { app }
